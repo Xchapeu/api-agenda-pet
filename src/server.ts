f0c55bd88465d55e0e6d8740f9e47a67621bedf1
xchapeu <xchapeu@gmail.com>
2023-04-06 11:17:56 -0300

    Add db.json

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const port = process.env.PORT || 8080;
+const router = jsonServer.router("db/db.json");
+
+// Make sure to use the default middleware
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(port, () => {
+ console.log("JSON Server is running on port: ", port);
+});
+
+// Export the Server API
+module.exports = server;
