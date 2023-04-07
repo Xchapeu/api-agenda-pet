64bd80bcfcaaf1263e5c327430adcead8da9d1f6
xchapeu <xchapeu@gmail.com>
2023-04-07 07:35:31 -0300

    Add readOnly option disabled

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ const port = process.env.PORT || 8080;
 const router = jsonServer.router("db.json");
 
 // Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ readonly: false });
 
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
- // Add custom route here if needed
+//  // Add custom route here if needed
  jsonServer.rewriter({
   "/api/*": "/$1",
  })
